3f7fcdc7d92c5d830e3e70d7b0765673d5a53756
DEIVA SIVAM <115927155+asdeiva@users.noreply.github.com>
2023-06-24 13:50:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,59 @@
 // Your code here.
+<script>
+  const itemsContainer = document.querySelector('.items');
+  let isDragging = false;
+  let startPosition = 0;
+  let currentTranslate = 0;
+  let prevTranslate = 0;
+  let animationId = 0;
+
+  itemsContainer.addEventListener('mousedown', dragStart);
+  itemsContainer.addEventListener('touchstart', dragStart);
+  itemsContainer.addEventListener('mouseup', dragEnd);
+  itemsContainer.addEventListener('touchend', dragEnd);
+  itemsContainer.addEventListener('mouseleave', dragEnd);
+  itemsContainer.addEventListener('mousemove', drag);
+  itemsContainer.addEventListener('touchmove', drag);
+
+  function dragStart(event) {
+    if (event.type === 'touchstart') {
+      startPosition = event.touches[0].clientX;
+    } else {
+      startPosition = event.clientX;
+    }
+    isDragging = true;
+    animationId = requestAnimationFrame(animation);
+    itemsContainer.classList.add('active');
+  }
+
+  function drag(event) {
+    if (!isDragging) return;
+
+    let currentPosition = 0;
+    if (event.type === 'touchmove') {
+      currentPosition = event.touches[0].clientX;
+    } else {
+      currentPosition = event.clientX;
+    }
+
+    currentTranslate = prevTranslate + currentPosition - startPosition;
+  }
+
+  function dragEnd() {
+    cancelAnimationFrame(animationId);
+    isDragging = false;
+    itemsContainer.classList.remove('active');
+    prevTranslate = currentTranslate;
+  }
+
+  function animation() {
+    setTransform(currentTranslate);
+    if (isDragging) {
+      requestAnimationFrame(animation);
+    }
+  }
+
+  function setTransform(translate) {
+    itemsContainer.style.transform = `translateX(${translate}px)`;
+  }
+</script>
